ccfcbb488cec442df1f615790ae9fef229c5c729
PhamHuy <163984306+PhamHuy092004@users.noreply.github.com>
2024-07-13 20:27:06 +0700

    Add project files.

---
+++ PhamQuocHuy_Report_RESTfulAPI/Controllers/ProductsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PhamQuocHuy_Report_RESTfulAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        //Tạo 1 hàm get mẫu chứa 2 sản phẩm
+        /// <summary>
+        /// Lấy danh sách tất cả sản phẩm.
+        /// </summary>
+        /// <returns>Danh sách sản phẩm.</returns>
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> GetAll()
+        {
+            return new string[] { "product1", "product2" };
+        }
+    }
+}
